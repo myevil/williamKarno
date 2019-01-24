61536f2cc32444d69da75269a4eff45a7496840b
william karno <williamkarno@gmail.com>
2019-01-24 21:00:49 +0700

    adding test snapshot

--- src/Component/Product/Item.spec.js
+++ src/Component/Product/Item.spec.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import ShoppingCart from './ShoppingCart';
+import Item from './Item';
 
 configure({ adapter: new Adapter() });
 
 describe('test hompage', () => {
-  const wrapper = shallow(<ShoppingCart
-    ShoppingCart={[]}
+  const wrapper = shallow(<Item
+    data={
+      {
+        data: {
+          image: '',
+          name: 'clothes',
+          price: '25',
+          currency: 'SGD',
+          quantity: 0,
+          total: 0,
+        }
+      }
+    }
   />)
   it('should render correctly', () => {
     expect(wrapper).toMatchSnapshot();
--- src/Component/Product/ListProduct.jsx
+++ src/Component/Product/ListProduct.jsx
@@ -1,40 +1,38 @@
 import React from 'react'
 import Item from './Item';
 
-class ListProduct extends React.PureComponent {
-  render() {
-    const { data, handleAddShoppingCartData } = this.props;
-    return (
-      <React.Fragment>
-        <div className={'scss-grid-md-3'}>
-          {data && <Item
-            data={data[1]}
-            handleAddShoppingCartData={handleAddShoppingCartData}
-          />}
-          {data && <Item
-            data={data[2]}
-            handleAddShoppingCartData={handleAddShoppingCartData}
-          />}
-        </div>
-        <div className='scss-grid-md-6'>
-          {data && <Item
-            data={data[0]}
-            handleAddShoppingCartData={handleAddShoppingCartData}
-          />}
-        </div>
-        <div className='scss-grid-md-3'>
-          {data && <Item
-            data={data[3]}
-            handleAddShoppingCartData={handleAddShoppingCartData}
-          />}
-          {data && <Item
-            data={data[4]}
-            handleAddShoppingCartData={handleAddShoppingCartData}
-          />}
-        </div>
-      </React.Fragment>
-    )
-  }
-}
+const ListProduct = React.memo(props => {
+  const { data, handleAddShoppingCartData } = props;
+  return (
+    <React.Fragment>
+      <div className={'scss-grid-md-3'}>
+        {data && <Item
+          data={data[1]}
+          handleAddShoppingCartData={handleAddShoppingCartData}
+        />}
+        {data && <Item
+          data={data[2]}
+          handleAddShoppingCartData={handleAddShoppingCartData}
+        />}
+      </div>
+      <div className='scss-grid-md-6'>
+        {data && <Item
+          data={data[0]}
+          handleAddShoppingCartData={handleAddShoppingCartData}
+        />}
+      </div>
+      <div className='scss-grid-md-3'>
+        {data && <Item
+          data={data[3]}
+          handleAddShoppingCartData={handleAddShoppingCartData}
+        />}
+        {data && <Item
+          data={data[4]}
+          handleAddShoppingCartData={handleAddShoppingCartData}
+        />}
+      </div>
+    </React.Fragment>
+  )
+})
 
 export default ListProduct
