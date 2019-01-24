4027f15da16975cc8ff346de84a8a53d81e66f87
william karno <williamkarno@gmail.com>
2019-01-24 21:17:42 +0700

    Updates

---
+++ precache-manifest.d73e2563b1b414cce51b18cce59b3c98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46567122290b11434ed3",
+    "url": "/williamKarno/static/js/runtime~main.46567122.js"
+  },
+  {
+    "revision": "541dcbc48d53aefe2253",
+    "url": "/williamKarno/static/js/main.541dcbc4.chunk.js"
+  },
+  {
+    "revision": "0a29ead9173a99ed3b8a",
+    "url": "/williamKarno/static/js/1.0a29ead9.chunk.js"
+  },
+  {
+    "revision": "541dcbc48d53aefe2253",
+    "url": "/williamKarno/static/css/main.26423744.chunk.css"
+  },
+  {
+    "revision": "e3b9c1b20800c1e78e246140302925b8",
+    "url": "/williamKarno/index.html"
+  }
+];
